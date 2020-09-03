3851990bd8ddd97bbd7313b0a2c6afb8eca8be7b
alexangria1992 <alex040892@gmail.com>
2020-09-03 23:13:19 +1200

    initial commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" type="text/css" href="fontawesome-free-5.14.0-web/fontawesome-free-5.14.0-web/css/all.css">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@700&display=swap" rel="stylesheet">
+
+    <link href="css/style.css" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>iSchool</title>
+    </head>
+  <body>
+    <!-- Start Navigation -->
+        <nav class="navbar navbar-expand-sm navbar-dark  pl-5 fixed-top">
+            <a class="navbar-brand" href="index.php">iSchool</a>
+            <span class="navbar-text">Learn and Implement</span>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" 
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <ul class="navbar-nav custom-nav pl-5">
+                    <li class="nav-item custom-nav-item"><a href="index.php" class="nav-link">Home</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Courses</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Payment</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">My Profile</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Logout</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Login</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Signup</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Feedback</a></li>
+                    <li class="nav-item custom-nav-item"><a href="" class="nav-link">Contact</a></li>
+                </ul>
+        </div>
+    </nav>
+
+    <!-- start video background--> 
+    <div class="container-fluid remove-vid-marg">
+        <div class="vid-parent">
+            <video playsinline autoplay muted loop>
+                <source src="video/banvid.mp4">
+            </video> 
+            <div class="vid-overlay"> </div>
+        </div>
+        <div class="vid-content">
+            <h1 class="my-content">Welcome to iSchool</h1>
+            <small class="my-content">Learn and Implement</small><br>
+            <a href="" class="btn btn-danger">Get Started</a>
+        </div>
+    </div>
+
+    <!-- Start Text Banner--> 
+    <div class="container-fluid bg-danger txt-banner">
+        <div class="row bottom-banner">
+            <div class="col-sm">
+                <h5><i class="fas fa-book-open mr-3"></i> 100+ Online courses</h5>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fas fa-users mr-3"></i>Expert Instructors</h5>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fas fa-keyboard mr-3"></i>Lifetime Access</h5>
+            </div>
+            <div class="col-sm">
+                <h5><i class="fas fa-rupee-sign mr-3"></i>money Back Guarantee*</h5>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
