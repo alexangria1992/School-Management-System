dbbd95020e92487cfaae43b56a18bf3c3fa7b8ce
alexangria1992 <alex040892@gmail.com>
2020-09-15 21:01:58 +1200

    Created Registration form with dynamic interation with database. Created a connection to the database. Chanaged the default slim jquery

---
+++ Student/addstudent.php
@@ -0,0 +1,19 @@
+<?php
+include_once("../dbConnection.php");
+if(isset($_POST['stusignup']) && isset($_POST['stupass']))
+{
+    $stuname = $_POST['stuname'];
+    $stuemail = $_POST['stuemail'];
+    $stupass = $_POST['stupass'];
+    $sql = "INSERT INTO student(stu_name, stu_email, stu_pass)VALUES('$stuname', '$stuemail', '$stupass')";
+
+    if($conn -> query($sql) == TRUE)
+    {
+        echo json_encode("OK");
+    }
+    else 
+    {
+        echo json_encode("Failed");
+    }
+}
+?>
--- mainInclude/footer.php
+++ mainInclude/footer.php
@@ -22,27 +22,12 @@
         <div class="modal-body">
         <!-- Start Student Registration form -->
 
-        <form>
-        <div class="form-group">
-            <i class="fas fa-user"></i><label for="stuname" class="pl-2 font-weight-bold">Name</label>
-            <input type="text" class="form-control" id="stuname" name="stuname"  placeholder="Name">
-        </div>
-        <div class="form-group">
-            <i class="fas fa-envelope"></i><label for="stuemail" class="pl-2 font-weight-bold">Email</label>
-            <input type="email" class="form-control" id="stuemail" name= "stueemail" placeholder="Email">
-            <small class="form-text">We'll never share your email with anyone else.</small>
-        </div>
-        <div class="form-group">
-            <i class="fas fa-key"></i><label for="stupass" class="pl-2 font-weight-bold">New Password</label>
-            <input type="password" class="form-control" id="stupass" name="stupass" placeholder="Password">
-           
-        </div>
-      
-      
-        </form>        
+         <?php include('studentRegistration.php')?>   
+             
         </div>
 
         <div class="modal-footer">
+            <span id="successMsg"></span>
             <button type="button" class="btn btn-primary" onclick="addStu()">Sign Up</button>
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
         </div>
@@ -154,8 +139,7 @@
     </div>
     </div>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="js/jquery.min.js" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="js/all.min.js"></script>
